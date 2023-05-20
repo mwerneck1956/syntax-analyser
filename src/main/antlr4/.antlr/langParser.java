ba8d98a4e9f8eec28cbb0b500443de2be6ecd37d
Matheus <matheus.werneck@compasso.com.br>
2023-05-20 18:45:56 -0300

    changed comment syntax

---
+++ src/main/antlr4/.antlr/langParser.java
@@ -0,0 +1,509 @@
+// Generated from c:\Users\Cliente\Desktop\UFJF\Compiladores\syntax-analyser\src\main\antlr4\lang.g4 by ANTLR 4.9.2
+ 
+	package com.compiler.grammar;
+	import com.compiler.ast.*;
+
+ 
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class langParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.9.2", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, T__7=8, T__8=9, 
+		T__9=10, T__10=11, ID=12, TYPE=13, INTEGER=14, FLOAT=15, BOOL=16, IF=17, 
+		THEN=18, ELSE=19, ITERATE=20, READ=21, PRINT=22, RETURN=23, CHAR=24, NEWLINE=25, 
+		WS=26, LINE_COMMENT=27, COMMENT=28;
+	public static final int
+		RULE_prog = 0, RULE_data = 1, RULE_typeDeclaration = 2, RULE_stmt = 3, 
+		RULE_expr = 4, RULE_term = 5, RULE_factor = 6;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"prog", "data", "typeDeclaration", "stmt", "expr", "term", "factor"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "';'", "'{'", "'}'", "'::'", "'='", "'?'", "'['", "']'", "':'", 
+			"'+'", "'*'", null, null, null, null, null, "'if'", "'then'", "'else'", 
+			"'iterate'", "'read'", "'print'", "'return'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, null, null, null, null, null, null, null, null, null, null, null, 
+			"ID", "TYPE", "INTEGER", "FLOAT", "BOOL", "IF", "THEN", "ELSE", "ITERATE", 
+			"READ", "PRINT", "RETURN", "CHAR", "NEWLINE", "WS", "LINE_COMMENT", "COMMENT"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "lang.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public langParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	public static class ProgContext extends ParserRuleContext {
+		public List<StmtContext> stmt() {
+			return getRuleContexts(StmtContext.class);
+		}
+		public StmtContext stmt(int i) {
+			return getRuleContext(StmtContext.class,i);
+		}
+		public ProgContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_prog; }
+	}
+
+	public final ProgContext prog() throws RecognitionException {
+		ProgContext _localctx = new ProgContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_prog);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(17); 
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			do {
+				{
+				{
+				setState(14);
+				stmt();
+				setState(15);
+				match(T__0);
+				}
+				}
+				setState(19); 
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+			} while ( (((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << ID) | (1L << INTEGER) | (1L << FLOAT))) != 0) );
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class DataContext extends ParserRuleContext {
+		public TerminalNode ID() { return getToken(langParser.ID, 0); }
+		public TypeDeclarationContext typeDeclaration() {
+			return getRuleContext(TypeDeclarationContext.class,0);
+		}
+		public DataContext data() {
+			return getRuleContext(DataContext.class,0);
+		}
+		public DataContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_data; }
+	}
+
+	public final DataContext data() throws RecognitionException {
+		DataContext _localctx = new DataContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_data);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(21);
+			match(ID);
+			setState(27);
+			_errHandler.sync(this);
+			switch (_input.LA(1)) {
+			case T__1:
+				{
+				setState(22);
+				match(T__1);
+				setState(23);
+				typeDeclaration();
+				setState(24);
+				match(T__2);
+				}
+				break;
+			case ID:
+				{
+				setState(26);
+				data();
+				}
+				break;
+			default:
+				throw new NoViableAltException(this);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class TypeDeclarationContext extends ParserRuleContext {
+		public TerminalNode ID() { return getToken(langParser.ID, 0); }
+		public TerminalNode TYPE() { return getToken(langParser.TYPE, 0); }
+		public TypeDeclarationContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_typeDeclaration; }
+	}
+
+	public final TypeDeclarationContext typeDeclaration() throws RecognitionException {
+		TypeDeclarationContext _localctx = new TypeDeclarationContext(_ctx, getState());
+		enterRule(_localctx, 4, RULE_typeDeclaration);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(29);
+			match(ID);
+			setState(30);
+			match(T__3);
+			setState(31);
+			match(TYPE);
+			setState(32);
+			match(T__0);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class StmtContext extends ParserRuleContext {
+		public TerminalNode ID() { return getToken(langParser.ID, 0); }
+		public ExprContext expr() {
+			return getRuleContext(ExprContext.class,0);
+		}
+		public List<StmtContext> stmt() {
+			return getRuleContexts(StmtContext.class);
+		}
+		public StmtContext stmt(int i) {
+			return getRuleContext(StmtContext.class,i);
+		}
+		public StmtContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_stmt; }
+	}
+
+	public final StmtContext stmt() throws RecognitionException {
+		StmtContext _localctx = new StmtContext(_ctx, getState());
+		enterRule(_localctx, 6, RULE_stmt);
+		try {
+			setState(54);
+			_errHandler.sync(this);
+			switch ( getInterpreter().adaptivePredict(_input,2,_ctx) ) {
+			case 1:
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(34);
+				match(ID);
+				setState(35);
+				match(T__4);
+				setState(36);
+				expr();
+				}
+				break;
+			case 2:
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(37);
+				expr();
+				setState(38);
+				match(T__5);
+				setState(39);
+				match(T__6);
+				setState(40);
+				stmt();
+				setState(41);
+				match(T__7);
+				setState(42);
+				match(T__8);
+				setState(43);
+				match(T__6);
+				setState(44);
+				stmt();
+				setState(45);
+				match(T__7);
+				}
+				break;
+			case 3:
+				enterOuterAlt(_localctx, 3);
+				{
+				setState(47);
+				expr();
+				setState(48);
+				match(T__5);
+				setState(49);
+				match(T__6);
+				setState(50);
+				stmt();
+				setState(51);
+				match(T__7);
+				}
+				break;
+			case 4:
+				enterOuterAlt(_localctx, 4);
+				{
+				setState(53);
+				expr();
+				}
+				break;
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class ExprContext extends ParserRuleContext {
+		public TermContext term() {
+			return getRuleContext(TermContext.class,0);
+		}
+		public ExprContext expr() {
+			return getRuleContext(ExprContext.class,0);
+		}
+		public ExprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_expr; }
+	}
+
+	public final ExprContext expr() throws RecognitionException {
+		ExprContext _localctx = new ExprContext(_ctx, getState());
+		enterRule(_localctx, 8, RULE_expr);
+		try {
+			setState(61);
+			_errHandler.sync(this);
+			switch ( getInterpreter().adaptivePredict(_input,3,_ctx) ) {
+			case 1:
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(56);
+				term();
+				setState(57);
+				match(T__9);
+				setState(58);
+				expr();
+				}
+				break;
+			case 2:
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(60);
+				term();
+				}
+				break;
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class TermContext extends ParserRuleContext {
+		public FactorContext factor() {
+			return getRuleContext(FactorContext.class,0);
+		}
+		public TermContext term() {
+			return getRuleContext(TermContext.class,0);
+		}
+		public TermContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_term; }
+	}
+
+	public final TermContext term() throws RecognitionException {
+		TermContext _localctx = new TermContext(_ctx, getState());
+		enterRule(_localctx, 10, RULE_term);
+		try {
+			setState(68);
+			_errHandler.sync(this);
+			switch ( getInterpreter().adaptivePredict(_input,4,_ctx) ) {
+			case 1:
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(63);
+				factor();
+				setState(64);
+				match(T__10);
+				setState(65);
+				term();
+				}
+				break;
+			case 2:
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(67);
+				factor();
+				}
+				break;
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class FactorContext extends ParserRuleContext {
+		public TerminalNode ID() { return getToken(langParser.ID, 0); }
+		public TerminalNode INTEGER() { return getToken(langParser.INTEGER, 0); }
+		public TerminalNode FLOAT() { return getToken(langParser.FLOAT, 0); }
+		public FactorContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_factor; }
+	}
+
+	public final FactorContext factor() throws RecognitionException {
+		FactorContext _localctx = new FactorContext(_ctx, getState());
+		enterRule(_localctx, 12, RULE_factor);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(70);
+			_la = _input.LA(1);
+			if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << ID) | (1L << INTEGER) | (1L << FLOAT))) != 0)) ) {
+			_errHandler.recoverInline(this);
+			}
+			else {
+				if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+				_errHandler.reportMatch(this);
+				consume();
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\36K\4\2\t\2\4\3\t"+
+		"\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\3\2\3\2\3\2\6\2\24\n\2\r\2"+
+		"\16\2\25\3\3\3\3\3\3\3\3\3\3\3\3\5\3\36\n\3\3\4\3\4\3\4\3\4\3\4\3\5\3"+
+		"\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5"+
+		"\3\5\5\59\n\5\3\6\3\6\3\6\3\6\3\6\5\6@\n\6\3\7\3\7\3\7\3\7\3\7\5\7G\n"+
+		"\7\3\b\3\b\3\b\2\2\t\2\4\6\b\n\f\16\2\3\4\2\16\16\20\21\2J\2\23\3\2\2"+
+		"\2\4\27\3\2\2\2\6\37\3\2\2\2\b8\3\2\2\2\n?\3\2\2\2\fF\3\2\2\2\16H\3\2"+
+		"\2\2\20\21\5\b\5\2\21\22\7\3\2\2\22\24\3\2\2\2\23\20\3\2\2\2\24\25\3\2"+
+		"\2\2\25\23\3\2\2\2\25\26\3\2\2\2\26\3\3\2\2\2\27\35\7\16\2\2\30\31\7\4"+
+		"\2\2\31\32\5\6\4\2\32\33\7\5\2\2\33\36\3\2\2\2\34\36\5\4\3\2\35\30\3\2"+
+		"\2\2\35\34\3\2\2\2\36\5\3\2\2\2\37 \7\16\2\2 !\7\6\2\2!\"\7\17\2\2\"#"+
+		"\7\3\2\2#\7\3\2\2\2$%\7\16\2\2%&\7\7\2\2&9\5\n\6\2\'(\5\n\6\2()\7\b\2"+
+		"\2)*\7\t\2\2*+\5\b\5\2+,\7\n\2\2,-\7\13\2\2-.\7\t\2\2./\5\b\5\2/\60\7"+
+		"\n\2\2\609\3\2\2\2\61\62\5\n\6\2\62\63\7\b\2\2\63\64\7\t\2\2\64\65\5\b"+
+		"\5\2\65\66\7\n\2\2\669\3\2\2\2\679\5\n\6\28$\3\2\2\28\'\3\2\2\28\61\3"+
+		"\2\2\28\67\3\2\2\29\t\3\2\2\2:;\5\f\7\2;<\7\f\2\2<=\5\n\6\2=@\3\2\2\2"+
+		">@\5\f\7\2?:\3\2\2\2?>\3\2\2\2@\13\3\2\2\2AB\5\16\b\2BC\7\r\2\2CD\5\f"+
+		"\7\2DG\3\2\2\2EG\5\16\b\2FA\3\2\2\2FE\3\2\2\2G\r\3\2\2\2HI\t\2\2\2I\17"+
+		"\3\2\2\2\7\25\358?F";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
