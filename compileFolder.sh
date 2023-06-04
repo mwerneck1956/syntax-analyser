3d4bc137d75b60526d77d546b0a06fd4cd87a640
Pedro <pedroalmeidavrb@gmail.com>
2023-06-04 13:18:15 -0300

    Criando script para testar os testes das pastas

--- compileFolder.sh
+++ compileFolder.sh
@@ -3,16 +3,13 @@ rm logs/application.log
 mvn -q antlr4:antlr4
 
 for file in $(find ./sintaxe/certo -name "*.lan"); do
-    mvn -q exec:java -Dexec.mainClass=com.compiler.App -Dexec.args="$file" >> testesCertos.txt
-    echo -e "\n------Proximoteste---------\n" >> testesCertos.txt
+    echo -e "Processing file: $file \n" >> ./resultados_testes/testesCertos.txt
+    mvn -q exec:java -Dexec.mainClass=com.compiler.App -Dexec.args="$file" >> ./resultados_testes/testesCertos.txt
+    echo -e "\n------Proximoteste---------\n" >> ./resultados_testes/testesCertos.txt
 done
 
 for file in $(find ./sintaxe/errado -name "*.lan"); do
-    mvn -q exec:java -Dexec.mainClass=com.compiler.App -Dexec.args="$file" >> testesErrados.txt
-    echo -e "\n------Proximoteste---------\n" >> testesErrados.txt
+    echo -e "Processing file: $file \n" >> ./resultados_testes/testesErrados.txt
+    mvn -q exec:java -Dexec.mainClass=com.compiler.App -Dexec.args="$file" >> ./resultados_testes/testesErrados.txt
+    echo -e "\n------Proximoteste---------\n" >> ./resultados_testes/testesErrados.txt
 done
-
-rm logs/application.log
-
-mvn -q antlr4:antlr4
-mvn exec:java -Dexec.mainClass=com.compiler.App -Dexec.args="./sintaxe/certo/attrADD.lan"
